f4ee416e44ee43d2924c51942df6425daac5a4a8
icesw <wsc011@ucsd.edu>
2016-01-20 22:03:25 -0800

    Added Answers to Exercise 1-3

--- tf_binding.sh
+++ tf_binding.sh
@@ -12,7 +12,13 @@
 cd /oasis/tscc/scratch/ucsd-train29
 mpirun -v -machinefile $PBS_NODEFILE -np 20 <./mpi.out>
 
-
+#Wendy Chen did exercise #1-3
+# Exercise 1
+awk -F "\t" '{ if ($4 == "nfkb") {print} }' tf.bed > tf.nfkb.bed
+# Exercise 2
+awk -F "\t" '{ if ($2 == "transcript") {print} }' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
+# Exercise 3
+bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
 # Audrey Hong did exercise #4,5
 # Exercise 4
